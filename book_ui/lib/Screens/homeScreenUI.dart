e4492ee34dc633a3a8c49e46adbe6973f7ca004b
Mayur Tikle <mayurtikle24@gmail.com>
2022-05-19 23:29:31 +0530

    created title text and textfield

--- book_ui/lib/Screens/homeScreenUI.dart
+++ book_ui/lib/Screens/homeScreenUI.dart
@@ -10,7 +10,8 @@ class HomeScreenUI extends StatelessWidget {
       child: Material(
         child: Scaffold(
           body: Container(
-            child: Column(children: [
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
               Padding(
                 padding:
                     const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
@@ -27,6 +28,55 @@ class HomeScreenUI extends StatelessWidget {
                       height: 35,
                     )
                   ],
+                ),
+              ),
+              SizedBox(height: 20),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  "Hey chromicle,",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87,
+                      fontSize: 28),
+                  textAlign: TextAlign.start,
+                ),
+              ),
+              SizedBox(height: 10),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  "Find a book you want to read",
+                  style: TextStyle(
+                      color: Color.fromARGB(255, 139, 136, 136), fontSize: 23),
+                  textAlign: TextAlign.start,
+                ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 20, 10, 10),
+                child: Container(
+                  height: 60,
+                  width: 350,
+                  decoration: BoxDecoration(
+                      color: Colors.grey[100],
+                      borderRadius: BorderRadius.circular(20),
+                      shape: BoxShape.rectangle),
+                  child: const Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 7),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            prefixIcon: Icon(
+                              CupertinoIcons.search,
+                              color: Colors.black87,
+                            ),
+                            hintText: "Search for anything",
+                            hintStyle: TextStyle(
+                                fontSize: 17,
+                                color: Color.fromARGB(255, 139, 136, 136)),
+                            border: InputBorder.none),
+                      )),
                 ),
               )
             ]),

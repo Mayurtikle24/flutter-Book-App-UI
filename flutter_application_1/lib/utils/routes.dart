b3b6c8673f3210f723450ad55dfaf16de72d8def
Mayur Tikle <mayurtikle24@gmail.com>
2022-05-20 20:19:18 +0530

    removed unused files

--- flutter_application_1/lib/utils/routes.dart
+++
@@ -1,6 +0,0 @@
-class MyRoutes {
-  static String logInRoute = "/login";
-  static String homeRoute = "/home";
-  static String homeDetailRoute = "/detailhome";
-  static String cartRoute = "/cart";
-}
